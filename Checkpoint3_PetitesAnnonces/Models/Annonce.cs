c73856bc7da5694e82b4c04bcf980be745c25f1f
adtautavel <adtautavel@yahoo.fr>
2021-01-08 10:31:49 +0100

    Add project files.

---
+++ Checkpoint3_PetitesAnnonces/Models/Annonce.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Checkpoint3_PetitesAnnonces.Models
+{
+    public class Annonce
+    {
+        public int Annonce_Id { get; set; }
+        public string Title { get; set; }
+        public string ObjectForSale { get; set; }
+        public string Description { get; set; }
+        public string Owner { get; set; }
+        public double Price { get; set; }
+        public DateTime Date { get; set; }
+
+
+    }
+}
